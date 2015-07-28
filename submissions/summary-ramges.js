a9b6d1c2df08ae2dab9c5519f44505fada808e40
xhu <xhu@freewheel.tv>
2015-07-28 15:22:24 +0800

    add the submission and program of summary ranges

---
+++ submissions/summary-ramges.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @return {string[]}
+ */
+var summaryRanges = function (nums) {
+  var result = [];
+  for (var i = 0, l = nums.length; i < l;) {
+    if (i === l - 1) {
+      result.push(nums[i].toString());
+      break;
+    } else {
+      for (var j = i + 1; j < l;) {
+        if (nums[j] - nums[j - 1] === 1)
+          j++;
+        else
+          break;
+      }
+      if (j === i + 1) {
+        result.push(nums[i].toString());
+      } else {
+        result.push(nums[i] + '->' + nums[j - 1]);
+      }
+      i = j;
+    }
+    console.log(i);
+  }
+  return result;
+};
