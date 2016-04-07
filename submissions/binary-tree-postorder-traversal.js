a7c29046142847d403a4d0b7cf3f4eddfd0e6ffd
xhu <hxtheone@gmail.com>
2016-04-08 01:45:34 +0800

    add two more solutions

---
+++ submissions/binary-tree-postorder-traversal.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var postorderTraversal = function(root) {
+    if (!root) return [];
+    var result = [];
+    var traversal = function (node) {
+        if (node.left) traversal(node.left);
+        if (node.right) traversal(node.right);
+        result.push(node.val);
+        return;
+    }
+    traversal(root);
+    return result;
+};
