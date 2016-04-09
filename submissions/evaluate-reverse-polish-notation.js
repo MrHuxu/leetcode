32e44c82c15325d03c9bb427df82e689b6ce2c90
xhu <hxtheone@gmail.com>
2016-04-09 22:20:36 +0800

    add four more submissions

---
+++ submissions/evaluate-reverse-polish-notation.js
@@ -0,0 +1,39 @@
+/**
+ * @param {string[]} tokens
+ * @return {number}
+ */
+var evalRPN = function(tokens) {
+  var stack = [];
+
+  var calc = function (operator) {
+    var num2 = stack.pop();
+    var num1 = stack.pop();
+    switch (tokens[i]) {
+      case '+':
+        stack.push(parseInt(num1 + num2));
+        break;
+
+      case '-':
+        stack.push(parseInt(num1 - num2));
+        break;
+
+      case '*':
+        stack.push(parseInt(num1 * num2));
+        break;
+
+      case '/':
+        stack.push(parseInt(num1 / num2));
+        break;
+    }
+  };
+
+  for (var i = 0, l = tokens.length; i < l; ++i) {
+    if (tokens[i].length === 1 && tokens[i].charCodeAt() >= 42 && tokens[i].charCodeAt() <= 47) {
+      calc(tokens[i]);
+    } else {
+      stack.push(parseInt(tokens[i]));
+    }
+  }
+
+  return stack.pop();
+};
---
+++ submissions/reverse-words-in-a-string.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} str
+ * @returns {string}
+ */
+var reverseWords = function(str) {
+  var arr = [], tmp;
+  for (var i = 0, l = str.length; i < l; ++i) {
+    if (str[i] !== ' ') {
+      tmp = tmp === undefined ? str[i] : tmp + str[i];
+    } else {
+      if (tmp !== undefined) {
+        arr.push(tmp);
+        tmp = undefined;
+      }
+    }
+  }
+  if (tmp) arr.push(tmp);
+
+  return arr.reverse().join(' ');
+};
