0e05a75cba1042a49344e16370ba12a2d5b278ea
xhu <hxtheone@gmail.com>
2016-04-12 23:15:27 +0800

    add three more submissions

---
+++ submissions/largest-number.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @return {string}
+ */
+var largestNumber = function(nums) {
+  nums.sort(function (a, b) {
+    var strA = a.toString();
+    var strB = b.toString();
+
+    var str1 = strA + strB;
+    var str2 = strB + strA;
+
+    if (str1 > str2)
+      return 1;
+    else if (str1 < str2)
+      return -1;
+    else
+      return 0;
+  }).reverse();
+
+  var i, len = nums.length;
+  for (i = 0; i < len; ++i) {
+    if (nums[i] !== 0 || i === len - 1) break;
+  }
+  return nums.slice(i).join('');
+};
