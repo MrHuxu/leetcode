a14554d0a2ba1bef5bcd58d14c3f7c10457bc701
xhu <hxtheone@gmail.com>
2016-04-12 23:26:52 +0800

    add another program & submission

---
+++ programs/repeated-dna-sequences.js
@@ -0,0 +1,19 @@
+var findRepeatedDnaSequences = function(s) {
+  var map = {}, result = [];
+  var len = s.length;
+
+  for (var i = 9; i < len; ++i) {
+    var subStr = s.slice(i - 9, i + 1);
+    if (map[subStr] === 1) {
+      result.push(subStr);
+    } else if (map[subStr] === undefined) {
+      map[subStr] = 0;
+    }
+    ++map[subStr];
+  }
+
+  return result;
+};
+
+console.log(findRepeatedDnaSequences("AAAAAAAAAAAAA"));
+console.log(findRepeatedDnaSequences("AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT"));
---
+++ submissions/repeated-dna-sequences.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} s
+ * @return {string[]}
+ */
+var findRepeatedDnaSequences = function(s) {
+  var map = {}, result = [];
+  var len = s.length;
+
+  for (var i = 9; i < len; ++i) {
+    var subStr = s.slice(i - 9, i + 1);
+    if (map[subStr] === 1) {
+      result.push(subStr);
+    } else if (map[subStr] === undefined) {
+      map[subStr] = 0;
+    }
+    ++map[subStr];
+  }
+
+  return result;
+};
