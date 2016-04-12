0db13e6f88cf70255cde1cb988420e93c640c984
xhu <hxtheone@gmail.com>
2016-04-12 23:55:28 +0800

    rename submissions

---
+++ submissions/058_length-of-last-word.js
@@ -0,0 +1,8 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+    var arr = s.split(' ').filter(function(e){return e.length > 0});
+    return arr.length > 0 ? arr[arr.length - 1].length : 0;
+};
---
+++ submissions/139_word-break.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} s
+ * @param {set<string>} wordDict
+ *   Note: wordDict is a Set object, see:
+ *   https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set
+ * @return {boolean}
+ */
+var wordBreak = function(s, wordDict) {
+  var dp = [];
+
+  for (var i = 0, l = s.length; i < l; ++i) {
+    var tmp = '';
+    for (var j = i; j >= 0; --j) {
+      tmp = s[j] + tmp;
+      if (j > 0) {
+        dp[i] = wordDict.has(tmp) && dp[j - 1];
+        if (dp[i]) break;
+      } else if (j === 0) {
+        dp[i] = wordDict.has(tmp);
+      }
+    }
+  }
+
+  return dp[s.length - 1];
+};
