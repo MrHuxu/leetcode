75eca65a106ee8ddd20a9f8603961a4a3c28b4a8
xhu <hxtheone@gmail.com>
2016-04-16 13:25:51 +0800

    add five more submissions

---
+++ submissions/206_reverse-linked-list.js
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+  var result = null;
+  while (head) {
+    var tmp = new ListNode(head.val);
+    tmp.next = result;
+    result = tmp;
+    head = head.next;
+  }
+  return result;
+};
