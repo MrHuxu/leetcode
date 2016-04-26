a25d57d5571119760c6bbe7a43cda1cabd8b3334
xhu <hxtheone@gmail.com>
2016-04-26 22:36:17 +0800

    add two more solutions

---
+++ submissions/268_missing-number.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function(nums) {
+    var len = nums.length;
+    var totalSum = (1 + len) * len / 2, sum = 0;
+    for (var i = 0; i < len; ++i) sum += nums[i];
+    return totalSum - sum;
+};
