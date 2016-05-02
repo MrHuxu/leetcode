4dc0bcc4cf1d9be3556fb47cb1dd654420dc510e
MrHuxu <hxtheone@gmail.com>
2016-05-02 16:28:09 +0800

    add another solution

---
+++ submissions/290_word-pattern.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} pattern
+ * @param {string} str
+ * @return {boolean}
+ */
+var wordPattern = function(pattern, str) {
+  var arr = str.split(' ');
+  var map = {}, reverseMap = {}, flag = arr.length === pattern.length;
+
+  if (flag) {
+    for (var i = 0, len = pattern.length; i < len; ++i) {
+      if (map[pattern[i]] === undefined && reverseMap[arr[i]] === undefined) {
+        map[pattern[i]] = arr[i];
+        reverseMap[arr[i]] = pattern[i];
+      } else {
+        flag = map[pattern[i]] === arr[i] && reverseMap[arr[i]] === pattern[i];
+        if (!flag) break;
+      }
+    }
+  }
+  
+  return flag;
+};
