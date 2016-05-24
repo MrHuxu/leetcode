901f7ab09b546e244ca2330e83936dbc326315eb
xhu <hxtheone@gmail.com>
2016-05-25 01:07:27 +0800

    add some comment to code

--- programs/maximum-product-of-word-lengths.js
+++ programs/maximum-product-of-word-lengths.js
@@ -4,7 +4,7 @@ var maxProduct = function(words) {
   words.forEach(function (word, index) {
     sums[index] = 0;
     for (var i = 0, len = word.length; i < len; ++i) {
-      sums[index] |= 1 << (word[i].charCodeAt() - 97);
+      sums[index] |= 1 << (word[i].charCodeAt() - 97); // use 26 bit length integer to store the existence of each letter
     }
     
     for (var i = 0; i <= index; ++i) {
