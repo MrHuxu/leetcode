ae1eca5d7c85f84b7ced957ac41b50acb814b7d6
xhu <hxtheone@gmail.com>
2016-05-26 23:07:43 +0800

    add another program and submission

---
+++ submissions/345_reverse-vowels-of-a-string.js
@@ -0,0 +1,34 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseVowels = function(s) {
+  var vowels = {
+    a: true,
+    e: true,
+    i: true,
+    o: true,
+    u: true,
+    A: true,
+    E: true,
+    I: true,
+    O: true,
+    U: true
+  };
+  
+  var str = s.split(''), tmp;
+  for (var i = 0, j = s.length - 1; i <= j;) {
+    if (vowels[str[i]] && vowels[str[j]]) {
+      tmp = str[i];
+      str[i] = str[j];
+      str[j] = tmp;
+      ++i;
+      --j;
+    } else {
+      if (!vowels[str[i]]) ++i;
+      if (!vowels[str[j]]) --j;
+    }
+  }
+  
+  return str.join('');
+};
