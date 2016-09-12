307a1e44331449d3564077ee47fdbcd3bedfe379
MrHuxu <hxtheone@gmail.com>
2016-09-12 17:22:22 +0800

    add another solution

---
+++ submissions/383_ransom-note.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} ransomNote
+ * @param {string} magazine
+ * @return {boolean}
+ */
+var canConstruct = function(ransomNote, magazine) {
+   var rPairs = {}, mPairs = {}, flag = true, i;
+   for (i = 0; i < ransomNote.length; ++i) {
+     rPairs[ransomNote[i]] = rPairs[ransomNote[i]] ? ++rPairs[ransomNote[i]] : 1;
+   }
+   for (i = 0; i < magazine.length; ++i) {
+     mPairs[magazine[i]] = mPairs[magazine[i]] ? ++mPairs[magazine[i]] : 1;
+   }
+
+   for (var key in rPairs) {
+     if (!mPairs[key] || rPairs[key] > mPairs[key]) {
+       flag = false;
+       break;
+     }
+   }
+
+   return flag;
+};
