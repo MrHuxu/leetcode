afd864aaf28dc1fc5500338184a392343adc369c
xhu <hxtheone@gmail.com>
2016-11-13 22:06:16 +0800

    add another solution

---
+++ programs/sum-of-left-leaves.js
@@ -0,0 +1,40 @@
+var sumOfLeftLeaves = function(root) {
+  var sum = 0;
+
+  // position: true => left, false => right
+  var visit = function (node, position) {
+    if (!node.left && !node.right && position) {
+      sum += node.val;
+    } else {
+      if (node.left) visit(node.left, true);
+      if (node.right) visit(node.right, false);
+    }
+  }
+  if (root) visit(root, false);
+
+  return sum;
+};
+
+var test1 = {
+  val: 3,
+  left: {
+    val: 9,
+    left: null,
+    right: null
+  },
+  right: {
+    val: 20,
+    left: {
+      val: 15,
+      left: null,
+      right: null
+    },
+    right: {
+      val: 7,
+      left: null,
+      right: null
+    }
+  }
+};
+
+console.log(sumOfLeftLeaves(test1));
---
+++ submissions/404_sum-of-left-leaves.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var sumOfLeftLeaves = function(root) {
+  var sum = 0;
+
+  // position: true => left, false => right
+  var visit = function (node, position) {
+    if (!node.left && !node.right && position) {
+      sum += node.val;
+    } else {
+      if (node.left) visit(node.left, true);
+      if (node.right) visit(node.right, false);
+    }
+  }
+  if (root) visit(root, false);
+
+  return sum;
+};
