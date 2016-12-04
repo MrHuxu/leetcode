e74bd0b9862646092df87ba7027399ef59b69c47
xhu <hxtheone@gmail.com>
2016-12-04 16:33:14 +0800

    add a solution and a tle solution

---
+++ programs/largest-divisible-subset.js
@@ -0,0 +1,34 @@
+var largestDivisibleSubset = function(nums) {
+  if (!nums.length) return [];
+
+  nums.sort(function (a, b) {
+    return a > b ? 1 : -1;
+  });
+
+  var maxLength = 0, dp = {};
+  nums.forEach(function (num, index) {
+    dp[num] = { max: 1 };
+    for (var i = index - 1; i >= 0; --i) {
+      if (num % nums[i] === 0 && dp[nums[i]].max >= dp[num].max - 1) {
+        dp[num].max = dp[nums[i]].max + 1;
+        dp[num].pre = i;
+      }
+    }
+    dp[num].arr = dp[num].pre === undefined ? [num] : [].concat(dp[nums[dp[num].pre]].arr, num);
+  });
+
+  var max = 0, result;
+  nums.forEach(function (num) {
+    if (dp[num].max > max) {
+      max = dp[num].max;
+      result = dp[num].arr;
+    }
+  });
+
+  return result;
+};
+
+console.log(largestDivisibleSubset([]));
+console.log(largestDivisibleSubset([1]));
+console.log(largestDivisibleSubset([1, 2, 3]));
+console.log(largestDivisibleSubset([1, 2, 4, 8]));
