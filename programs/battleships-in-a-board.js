5d7085b3e8a009cfff41b447756cfd8c26b465bd
xhu <hxtheone@gmail.com>
2016-12-11 00:33:01 +0800

    add program and solution for problem 419

--- programs/battleships-in-a-board.js
+++ programs/battleships-in-a-board.js
@@ -1,3 +1,24 @@
 var countBattleships = function(board) {
-  
+  var result = 0;
+  var m = board.length, n = board[0] && board[0].length, i, j;
+
+  var check = function (i, j) {
+    return (board[i - 1] && board[i - 1][j] === 'X') || (board[i][j - 1] && board[i][j - 1] === 'X');
+  }
+
+  for (i = 0; i < m; ++i) {
+    for (j = 0; j < n; ++j) {
+      if (board[i][j] === 'X' && !check(i, j)) ++result;
+    }
+  }
+
+  return result;
 };
+
+console.log(countBattleships([
+  ['X', '.', '.', 'X'],
+  ['.', '.', '.', 'X'],
+  ['.', '.', '.', 'X'],
+  ['.', '.', '.', 'X']
+]));
+console.log(countBattleships(['X', 'X', 'X']));
---
+++ submissions/419_battleships-in-a-board.js
@@ -0,0 +1,20 @@
+/**
+ * @param {character[][]} board
+ * @return {number}
+ */
+var countBattleships = function(board) {
+  var result = 0;
+  var m = board.length, n = board[0] && board[0].length, i, j;
+
+  var check = function (i, j) {
+    return (board[i - 1] && board[i - 1][j] === 'X') || (board[i][j - 1] && board[i][j - 1] === 'X');
+  }
+
+  for (i = 0; i < m; ++i) {
+    for (j = 0; j < n; ++j) {
+      if (board[i][j] === 'X' && !check(i, j)) ++result;
+    }
+  }
+
+  return result;
+};
