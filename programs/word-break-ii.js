9825311b483f40d96a85e2010f7961fad0ed64c0
MrHuxu <hxtheone@gmail.com>
2016-12-28 18:18:01 +0800

    add a draft of word break ii

---
+++ programs/word-break-ii.js
@@ -0,0 +1,38 @@
+var wordBreak = function (s, wordDict) {
+  var dictMap = {};
+  wordDict.forEach(function (word) {
+    dictMap[word] = true;
+  });
+
+  var dp = [[{ arr: [], prefix: '' }]];
+  for (var i = 1; i <= s.length; ++i) {
+    var letter = s[i - 1];
+    var prev = dp[i - 1];
+    var curr = [];
+
+    prev.forEach(function (p) {
+      var word = p.prefix + letter;
+      curr.push({ arr: p.arr, prefix: word });
+
+      var tmp = p.arr.slice();
+      if (dictMap[word]) {
+        tmp.push(word);
+        curr.push({ arr: tmp, prefix: '' });
+      }
+    });
+    dp.push(curr);
+  }
+
+  var result = [];
+  dp[s.length].forEach(function (d) {
+    if (!d.prefix.length) {
+      result.push(d.arr.join(' '));
+    }
+  });
+
+  return result;
+};
+
+console.log(wordBreak('catsanddog', ["cat", "cats", "and", "sand", "dog"]));
+console.log(wordBreak("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+["a","aa","aaa","aaaa","aaaaa","aaaaaa","aaaaaaa","aaaaaaaa","aaaaaaaaa","aaaaaaaaaa"]))
