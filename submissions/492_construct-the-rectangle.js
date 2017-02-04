ee4b8378e56f09fb1005e99826b8d04e330820c5
MrHuxu <hxtheone@gmail.com>
2017-02-04 14:02:42 +0800

    add the program and solution of 492

---
+++ submissions/492_construct-the-rectangle.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} area
+ * @return {number[]}
+ */
+var constructRectangle = function (area) {
+  var num = parseInt(Math.sqrt(area));
+
+  if (num * num === area) {
+    return [num, num];
+  } else {
+    var l, w;
+    while (num++) {
+      if (area % num === 0) return [num, area / num];
+    }
+  }
+};
