cff2f6b0a1945f3f9030ba56e6b53daef14187a2
MrHuxu <hxtheone@gmail.com>
2017-02-04 15:37:26 +0800

    add program and solution for problem 485

---
+++ submissions/485_max-consecutive-ones.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMaxConsecutiveOnes = function (nums) {
+  var max = 0, curr = 0;
+
+  nums.forEach(function (num) {
+    if (num === 1) {
+      curr += 1;
+      max = Math.max(max, curr);
+    } else {
+      curr = 0;
+    }
+  });
+
+  return max;
+};
