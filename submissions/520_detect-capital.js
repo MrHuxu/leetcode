65ff6569bef7b27dd465a8001af3f691cc517438
MrHuxu <hxtheone@gmail.com>
2017-03-14 14:15:44 -0400

    add problem and solution of 520

---
+++ submissions/520_detect-capital.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} word
+ * @return {boolean}
+ */
+var detectCapitalUse = function (word) {
+  const upperChars = { A: true, B: true, C: true, D: true, E: true, F: true, G: true, H: true, I: true, J: true, K: true, L: true, M: true, N: true, O: true, P: true, Q: true, R: true, S: true, T: true, U: true, V: true, W: true, X: true, Y: true, Z: true };
+
+  var capitalAmount = 0;
+  for (var i = 0; i < word.length; ++i) {
+    if (upperChars[word[i]]) ++capitalAmount;
+  }
+
+  return capitalAmount === word.length || !capitalAmount || !!(capitalAmount === 1 && upperChars[word[0]]);
+};
