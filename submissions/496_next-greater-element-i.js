e9e063f9246b4556811f5c67cc165ea50fc720bd
MrHuxu <hxtheone@gmail.com>
2017-04-18 17:20:24 -0400

    add program and solution for 496

---
+++ submissions/496_next-greater-element-i.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} findNums
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var nextGreaterElement = function (findNums, nums) {
+  var next = {};
+  for (var i = 0; i < nums.length; ++i) {
+    next[nums[i]] = -1;
+    for (var j = 0; j < i; ++j) {
+      if (nums[j] < nums[i] && -1 === next[nums[j]]) {
+        next[nums[j]] = nums[i];
+      }
+    }
+  }
+
+  return findNums.map(function(findNum) {
+    return next[findNum];
+  });
+};
