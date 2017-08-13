cec41d143c159195886ef3f650ad09a5522de79f
xhu <hxtheone@gmail.com>
2017-08-13 23:51:22 +0800

    add the program and solution of problem 646

---
+++ submissions/646_maximum-length-of-pair-chain.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[][]} pairs
+ * @return {number}
+ */
+var findLongestChain = function(pairs) {
+  if (!pairs.length) return 0;
+  pairs.sort((a, b) => a[0] - b[0]);
+
+  let dp = [];
+  for (let i = 0; i < pairs.length; ++i) dp[i] = 1;
+
+  for (let i = 1; i < pairs.length; ++i) {
+    for (let j = 0; j < i; ++j) {
+      if (pairs[i][0] > pairs[j][1] && dp[i] < dp[j] + 1) dp[i] = dp[j] + 1;
+    }
+  }
+
+  return Math.max(...dp);
+};
