f12c5d1573609ae800bb472911d644280bcdd053
xhu <hxtheone@gmail.com>
2017-08-14 23:57:01 +0800

    add the program and solution of problem 453

---
+++ submissions/453_minimum-moves-to-equal-array-elements.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minMoves = function(nums) {
+  let min = sum = nums[0];
+  for (let i = 1; i < nums.length; ++i) {
+    sum += nums[i];
+    min = Math.min(min, nums[i]);
+  }
+
+  return sum - min * nums.length;
+};
