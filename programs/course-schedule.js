2055cf521d391709da02dd066f857580b4a87903
MrHuxu <hxtheone@gmail.com>
2017-09-11 23:34:32 +0800

    add program and solution for problem 207

--- programs/course-schedule.js
+++ programs/course-schedule.js
@@ -1,5 +1,43 @@
+/**
+ * @param {number} numCourses
+ * @param {number[][]} prerequisites
+ * @return {boolean}
+ */
 var canFinish = function(numCourses, prerequisites) {
-  
+  const graph = prerequisites.reduce((prev, curr) => {
+    prev[curr[0]] = prev[curr[0]] ? [...prev[curr[0]], curr[1]] : [curr[1]];
+    return prev;
+  }, {});
+
+  let onStack = {};
+  let marked = {};
+  let flag = true;
+
+  const dfs = v => {
+    onStack[v] = true;
+    marked[v] = true;
+    if (graph[v]) {
+      for (let w of graph[v]) {
+        if (!flag) return;
+        else if (!marked[w]) dfs(w);
+        else if (onStack[w]) {
+          flag = false;
+          return;
+        }
+      }
+    }
+    onStack[v] = false;
+  };
+
+  Object.keys(graph).forEach(v => {
+    if (flag && !marked[v]) {
+      dfs(v);
+    }
+  });
+
+  return flag;
 };
 
-console.log(canFinish(3, [[0, 1], [0, 2]]));
+console.log(canFinish(2, [[1,0]]));
+console.log(canFinish(2, [[1,0],[0,1]]));
+console.log(canFinish(3, [[0,1],[0,2],[1,2]]));
