7a2d5a00a21e2d984d03c6dcfb2e353a9f937178
MrHuxu <hxtheone@gmail.com>
2017-09-21 17:35:42 +0800

    add the program and submission of problem 10

--- programs/regular-expression-matching.js
+++ programs/regular-expression-matching.js
@@ -4,23 +4,43 @@
  * @return {boolean}
  */
 var isMatch = function(s, p) {
-  let result = false;
+  let patterns = [];
 
+  for (let i = 0; i < p.length; ++i) {
+    if ('*' === p[i]) {
+      patterns[patterns.length - 1].unlimit = true;
+    } else {
+      patterns.push({ ch: p[i] });
+    }
+  }
+
+  if (
+    patterns.length &&
+    ((!patterns[0].unlimit && patterns[0].ch !== s[0] && patterns[0].ch !== '.') ||
+    (!patterns[patterns.length - 1].unlimit && patterns[patterns.length - 1].ch !== s[s.length - 1] && patterns[patterns.length - 1].ch !== '.'))
+  ) {
+    return false;
+  }
+
+  let result = false;
   const traverse = (i, j) => {
-    if (i === s.length && j === p.length) {
-      result = true;
-      return;
+    if (i === s.length) {
+      while (patterns[j] && patterns[j].unlimit) ++j;
+      if (j === patterns.length) {
+        result = true;
+        return;
+      }
     }
 
-    if (result || i === s.length || j === p.length) return;
+    if (result || j === patterns.length) return;
 
-    if (s[i] === p[j] || '.' === p[j]) {
+    if (s[i] && patterns[j] && (s[i] === patterns[j].ch || '.' === patterns[j].ch)) {
       traverse(i + 1, j + 1);
-    } else if ('*' === p[j]) {
-      traverse(i + 1, j);
-      if (s[i] === p[j + 1]) traverse(i, j + 1);
-      if (s[i + 1] === p[j + 1]) traverse(i + 1, j + 1);
-    } else if (s[i] !== p[j]) {
+      if (patterns[j].unlimit) {
+        traverse(i + 1, j);
+        traverse(i, j + 1);
+      }
+    } else if (patterns[j].unlimit) {
       traverse(i, j + 1);
     }
   };
@@ -29,11 +49,18 @@ var isMatch = function(s, p) {
   return result;
 };
 
-console.log(isMatch('aa', 'a'));
-console.log(isMatch('aa', 'aa'));
-console.log(isMatch('aaa', 'a'));
-console.log(isMatch('aa', 'a*'));
-console.log(isMatch('aa', '.*'));
-console.log(isMatch('ab', '.*'));
-console.log(isMatch('aab', 'c*a*b'));
-console.log(isMatch('abcd', 'd*'));
+console.log(isMatch('aa', 'a')); // false
+console.log(isMatch('aa', 'aa')); // true
+console.log(isMatch('aaa', 'a')); // false
+console.log(isMatch('aa', 'a*')); // true
+console.log(isMatch('aa', '.*')); // true
+console.log(isMatch('ab', '.*')); // true
+console.log(isMatch('aab', 'c*a*b')); // true
+console.log(isMatch('abcd', 'd*')); // false
+console.log(isMatch('ab', '.*c')); // false
+console.log(isMatch('ab', '.*c')); // false
+console.log(isMatch('a', 'ab*')); // true
+console.log(isMatch('', '..*')); // false
+console.log(isMatch('bbbba', '.*a*a')); // true
+console.log(isMatch('aaaaaaaaaaaaab', 'a*a*a*a*a*a*a*a*a*a*c')); // false
+console.log(isMatch('a', ''));
