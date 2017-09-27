3a24e98a9d55ea68c31d9d83fb567b7e038b8d97
MrHuxu <hxtheone@gmail.com>
2017-09-27 14:07:45 +0800

    add the program and submission of problem 673

--- programs/number-of-longest-increasing-subsequence.js
+++ programs/number-of-longest-increasing-subsequence.js
@@ -3,25 +3,29 @@
  * @return {number}
  */
 var findNumberOfLIS = function(nums) {
-  const dp = [], result = [0];
+  const dp = [], result = [0, 0];
   for (let i = 0; i < nums.length; ++i) {
-    dp[i] = 1;
-    result[dp[i]] = result[dp[i]] ? result[dp[i]] + 1 : 1;
-
+    let max = 1, tmp = { 1: 1 };
     for (let j = 0; j < i; ++j) {
       if (nums[i] > nums[j]) {
-        dp[i] = Math.max(dp[i], dp[j] + 1);
-        result[dp[i]] = result[dp[i]] ? result[dp[i]] + 1 : 1;
+        max = Math.max(max, dp[j][0] + 1);
+        if (max === dp[j][0] + 1) {
+          tmp[max] = tmp[max] ? tmp[max] + dp[j][1] : dp[j][1];
+        }
       }
     }
+    dp[i] = [max, tmp[max]];
+    result[max] = result[max] ? result[max] + tmp[max] : tmp[max];
   }
   console.log(dp, result);
 
   return result.pop();
 };
 
-// console.log(findNumberOfLIS([]));
-// console.log(findNumberOfLIS([1]));
-// console.log(findNumberOfLIS([1, 3, 5, 4, 7]));
+console.log(findNumberOfLIS([]));
+console.log(findNumberOfLIS([1]));
+console.log(findNumberOfLIS([1, 3, 5, 4, 7]));
+console.log(findNumberOfLIS([1,2,4,3,5]));
 console.log(findNumberOfLIS([1,2,4,3,5,4,7,2]));
-// console.log(findNumberOfLIS([2, 2, 2, 2, 2]));
+console.log(findNumberOfLIS([2, 2, 2, 2, 2]));
+console.log(findNumberOfLIS([100,90,80,70,60,50,60,70,80,90,100]));
