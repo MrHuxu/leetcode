64ffaedd07e7e8375ab8071860501335a1e7441a
MrHuxu <hxtheone@gmail.com>
2017-09-29 04:08:45 +0800

    move cases from program to problem

--- problems/530_minimum-absolute-difference-in-bst/test-cases.js
+++ problems/530_minimum-absolute-difference-in-bst/test-cases.js
@@ -1,1 +1,6 @@
-module.exports = [];
+const { buildTree } = require('../../scripts/problem-utils');
+
+module.exports = [{
+  input  : [buildTree([2404, 687, 4310, 3, null, 3395])],
+  output : 684
+}];
