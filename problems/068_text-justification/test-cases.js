7af50a4298aea21b5eeef98c07b5f2f4acbf0629
MrHuxu <hxtheone@gmail.com>
2017-10-23 03:10:41 +0800

    add the program and solution of problem 68

---
+++ problems/068_text-justification/test-cases.js
@@ -0,0 +1,8 @@
+module.exports = [{
+  input  : [["This", "is", "an", "example", "of", "text", "justification."], 16],
+  output : [
+    "This    is    an",
+    "example  of text",
+    "justification.  "
+  ]
+}];
