043ec619d7c08dbad5d7c2bb91822f161f003a4f
MrHuxu <hxtheone@gmail.com>
2017-10-24 15:36:36 +0800

    add the solution and test cases for problem 714

---
+++ problems/714_best-time-to-buy-and-sell-stock-with-transaction-fee/index.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} prices
+ * @param {number} fee
+ * @return {number}
+ */
+var maxProfit = function(prices, fee) {
+  let result = 0, minBuy = prices[0], maxSell = null;
+
+  for (let price of prices) {
+    if (maxSell !== null && maxSell - price >= fee) {
+      result += (maxSell - minBuy >= fee) ? maxSell - minBuy - fee : 0;
+      minBuy = price;
+      maxSell = null;
+    } else if (price <= minBuy) {
+      minBuy = price;
+      maxSell = null;
+    } else if (null === maxSell || (maxSell !== null && price >= maxSell)) {
+      maxSell = price;
+    } 
+  }
+  result += (maxSell !== null && maxSell - minBuy >= fee) ? maxSell - minBuy - fee : 0;
+
+  return result;
+};
+
+module.exports = maxProfit;
---
+++ problems/714_best-time-to-buy-and-sell-stock-with-transaction-fee/test-cases.js
@@ -0,0 +1,22 @@
+module.exports = [{
+  input  : [[1, 3, 2, 8, 4, 9], 2],
+  output : 8
+}, {
+  input  : [[], 2],
+  output : 0
+}, {
+  input  : [[1, 2], 1],
+  output : 0
+}, {
+  input  : [[1, 3], 1],
+  output : 1
+}, {
+  input  : [[9,8,7,1,2], 3],
+  output : 0
+}, {
+  input  : [[4,5,2,4,3,3,1,2,5,4],1],
+  output : 4
+}, {
+  input  : [[2,1,4,4,2,3,2,5,1,2], 1],
+  output : 4
+}];
