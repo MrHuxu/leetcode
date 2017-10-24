22be02c46f0aeaa6c56238d2ff87c765b04649c2
MrHuxu <hxtheone@gmail.com>
2017-10-24 16:29:26 +0800

    add the solution and test cases for problem 713

---
+++ problems/713_subarray-product-less-than-k/index.js
@@ -0,0 +1,35 @@
+/**
+* Problem: https://leetcode.com/problems/subarray-product-less-than-k/description/
+*/
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var numSubarrayProductLessThanK = function(nums, k) {
+  return nums.reduce((prev, num) => {
+    let dp = new Map(), { dp: prevDp, count } = prev;
+
+    const keyIt = prevDp.keys();
+    let key = keyIt.next();
+
+    while (!key.done) {
+      const prd = key.value * num;
+      const inc = prevDp.has(key.value) ? prevDp.get(key.value) : 1;
+      if (prd < k) {
+        dp.set(prd, inc);
+        count += inc;
+      }
+      key = keyIt.next();
+    }
+
+    if (num < k) {
+      dp.set(num, dp.has(num) ? dp.get(num) + 1 : 1);
+      count += 1;
+    }
+
+    return { dp, count };
+  }, { dp: new Map(), count: 0 }).count;
+};
+
+module.exports = numSubarrayProductLessThanK;
