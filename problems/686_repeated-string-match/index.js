693ae3e47db329333aa87938fff16b09c6ca0a92
MrHuxu <hxtheone@gmail.com>
2017-11-09 19:04:22 +0800

    add the code and test cases for problem 686

---
+++ problems/686_repeated-string-match/index.js
@@ -0,0 +1,24 @@
+/**
+ * Problem: https://leetcode.com/problems/repeated-string-match/description/
+ */
+/**
+ * @param {string} A
+ * @param {string} B
+ * @return {number}
+ */
+var repeatedStringMatch = function(A, B) {
+  let result = 1, str = A, found = false;
+  do {
+    if (str.includes(B)) {
+      found = true;
+      break;
+    } else {
+      str += A;
+      result++;
+    }
+  } while (str.length <= B.length * 2);
+
+  return found ? result : - 1;
+};
+
+module.exports = repeatedStringMatch;
---
+++ problems/686_repeated-string-match/test-cases.js
@@ -0,0 +1,13 @@
+module.exports = [{
+  input  : ['abcd', 'cdabcdab'],
+  output : 3
+}, {
+  input  : ['aaaa', 'a'],
+  output : 1
+}, {
+  input  : ["bb", "bbbbbbb"],
+  output : 4
+}, {
+  input  : ["abababaaba", "aabaaba"],
+  output : 2
+}];
