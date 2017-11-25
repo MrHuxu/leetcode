3751281376a29881e6fa7c6fd9322e52d4bff584
MrHuxu <hxtheone@gmail.com>
2017-11-25 23:42:29 +0800

    add the code and test cases for 637

---
+++ problems/637_average-of-levels-in-binary-tree/index.js
@@ -0,0 +1,41 @@
+/**
+ * Problem: https://leetcode.com/problems/average-of-levels-in-binary-tree/description/
+ */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var averageOfLevels = function(root) {
+  if (!root) return [];
+
+  const result = [];
+  let nodes = [root], sum = root.val;
+  while (nodes.length) {
+    result.push(sum / nodes.length);
+
+    const nextNodes = [];
+    sum = 0;
+    for (let node of nodes) {
+      if (node.left) {
+        nextNodes.push(node.left);
+        sum += node.left.val;
+      }
+      if (node.right) {
+        nextNodes.push(node.right);
+        sum += node.right.val;
+      }
+    }
+    nodes = nextNodes;
+  }
+
+  return result;
+};
+
+module.exports = averageOfLevels;
