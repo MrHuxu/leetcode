ed1dcadaeaf2de2c5273428ff5f94e05aa340eee
MrHuxu <hxtheone@gmail.com>
2018-01-04 01:32:31 +0800

    update the code and the test cases for question 718

--- problems/712_minimum-ascii-delete-sum-for-two-strings/index.js
+++ problems/712_minimum-ascii-delete-sum-for-two-strings/index.js
@@ -9,12 +9,8 @@
 const minimumDeleteSum = function(s1, s2) {
   let min = null;
   dp = [[0]];
-  for (let i = 1; i <= s1.length; i++) {
-    dp[i] = [dp[i - 1][0] + s1[i - 1].charCodeAt()];
-  }
-  for (let i = 1; i <= s2.length; i++) {
-    dp[0][i] = dp[0][i - 1] + s2[i - 1].charCodeAt();
-  }
+  for (let i = 1; i <= s1.length; i++) dp[i] = [dp[i - 1][0] + s1[i - 1].charCodeAt()];
+  for (let i = 1; i <= s2.length; i++) dp[0][i] = dp[0][i - 1] + s2[i - 1].charCodeAt();
 
   for (let i = 1; i <= s1.length; i++) {
     for (let j = 1; j <= s2.length; j++) {
--- problems/718_maximum-length-of-repeated-subarray/index.js
+++ problems/718_maximum-length-of-repeated-subarray/index.js
@@ -6,33 +6,19 @@
  * @param {number[]} B
  * @return {number}
  */
-var findLength = function(A, B) {
-  let max = 0;
+const findLength = function(A, B) {
+  const dp = [[0]];
+  for (let i = 1; i <= B.length; i++) dp[0][i] = 0;
 
-  const dfs = (i, j, len) => {
-    if (A[i] === undefined || B[j] === undefined) {
-      max = Math.max(max, len);
-    } else {
-      let m = i, n = j, o = len;
-      if (A[i] === B[j]) {
-        while (A[m] !== undefined && B[n] !== undefined && A[m] === B[n]) {
-          m++;
-          n++;
-          o++;
-        }
-        dfs(m, n, o);
-      } else {
-        max = Math.max(max, len);
-      }
-      if (Math.max(A.length - i, B.length - j) > Math.max(o, max)) {
-        dfs(i + 1, j, 0);
-        dfs(i, j + 1, 0);
-      }
+  let result = 0;
+  for (let i = 1; i <= A.length; i++) {
+    dp[i] = [0];
+    for (let j = 1; j <= B.length; j++) {
+      dp[i][j] = A[i - 1] === B[j - 1] ? (dp[i - 1][j - 1] + 1) : 0;
+      result = Math.max(result, dp[i][j]);
     }
-  };
-  dfs(0, 0, 0);
-
-  return max;
+  }
+  return result;
 };
 
 module.exports = findLength;
