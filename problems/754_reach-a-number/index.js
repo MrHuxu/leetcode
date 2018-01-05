b624f1f022422d627c94661cf1d2b4abab5dfd12
MrHuxu <hxtheone@gmail.com>
2018-01-06 02:24:26 +0800

    use n instead of step in the code of 754

--- problems/754_reach-a-number/index.js
+++ problems/754_reach-a-number/index.js
@@ -4,10 +4,10 @@
 
 const reachANumber = target => {
   target = Math.abs(target);
-  for (let step = 1; true; step++) {
-    const sum = (step + step * step) / 2;
+  for (let n = 1; true; n++) {
+    const sum = (n + n * n) / 2;
     if (sum === target || (sum > target && 0 === (sum - target) % 2))
-      return step;
+      return n;
   }
 };
 
