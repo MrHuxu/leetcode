5e5e4cdf7297bd8fefbd5117badb1fe76cefb567
MrHuxu <hxtheone@gmail.com>
2018-02-17 23:57:36 +0800

    add the code and test cases for 319

---
+++ problems/319_bulb-switcher/index.js
@@ -0,0 +1,7 @@
+/**
+ * Problem: https://leetcode.com/problems/bulb-switcher/description/
+ */
+
+const bulbSwitcher = n => parseInt(Math.sqrt(n));
+
+module.exports = bulbSwitcher;
---
+++ problems/319_bulb-switcher/test-cases.js
@@ -0,0 +1,7 @@
+module.exports = [{
+  input  : [3],
+  output : 1
+}, {
+  input  : [10000000],
+  output : 3162
+}];
