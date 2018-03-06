daaeebec1ea5b2d70e2db874ba1131de43fca256
xhu <hxtheone@gmail.com>
2018-03-06 23:21:13 +0800

    update the code and the test cases for question 792

--- problems/792_number-of-matching-subsequences/index.js
+++ problems/792_number-of-matching-subsequences/index.js
@@ -3,36 +3,28 @@
  */
 
 const numberOfMatchingSubsequences = (S, words) => {
-  const TrieNode = function () {
-    this.endCount = 0;
-    this.children = {};
-  };
-  const root = new TrieNode();
-  for (let word of words) {
-    let tmp = root;
-    for (let i = 0; i < word.length; i++) {
-      const ch = word[i];
-      if (!tmp.children[ch]) tmp.children[ch] = new TrieNode(false);
-      tmp = tmp.children[ch];
+  const visit = words.reduce((pre, curr) => {
+    if (pre[curr]) {
+      pre[curr][1]++;
+    } else {
+      pre[curr] = [0, 1];
     }
-    tmp.endCount++;
-  }
+    return pre;
+  }, {});
 
   let result = 0;
-  const traverse = (n, node) => {
-    const ch = S[n];
-    if (!node.children[ch]) return;
-    
-    node = node.children[ch];
-    if (node.endCount) {
-      result += node.endCount;
-      node.endCount = 0;
+  for (let i = 0; i < S.length; i++) {
+    const ch = S[i];
+    for (let key of Object.keys(visit)) {
+      if (ch === key[visit[key][0]]) {
+        visit[key][0]++;
+        if (visit[key][0] === key.length) {
+          result += visit[key][1];
+          delete visit[key];
+        }
+      }
     }
-    for (let i = n + 1; i < S.length; i++) {
-      traverse(i, node);
-    }
-  };
-  for (let i = 0; i < S.length; i++) traverse(i, root);
+  }
 
   return result;
 };
