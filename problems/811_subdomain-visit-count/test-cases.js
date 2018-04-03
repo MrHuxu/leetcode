cda448528fbf8a6148a00657442862399d664a57
didi <didi@bogon.didichuxing.com>
2018-04-03 19:14:51 +0800

    add the code and the test cases of question 811, and update case runner

---
+++ problems/811_subdomain-visit-count/test-cases.js
@@ -0,0 +1,9 @@
+module.exports = [{
+  noOrder : true,
+  input   : [['9001 discuss.leetcode.com']],
+  output  : ['9001 discuss.leetcode.com', '9001 leetcode.com', '9001 com']
+}, {
+  noOrder : true,
+  input   : [['900 google.mail.com', '50 yahoo.com', '1 intel.mail.com', '5 wiki.org']],
+  output  : ['901 mail.com','50 yahoo.com','900 google.mail.com','5 wiki.org','5 org','1 intel.mail.com','951 com']
+}];
