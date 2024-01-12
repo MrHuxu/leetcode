55ecca315ae9176f08bb0b56ca2e67c317e44dcd
xhu <hxtheone@gmail.com>
2024-01-12 10:49:49 +0800

    completed daily challenge on 2024-01-12

---
+++ problems/1704_determine-if-string-halves-are-alike/__main__.py
@@ -0,0 +1,6 @@
+vowels = set(['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'])
+
+class Solution:
+    def halvesAreAlike(self, s: str) -> bool:
+        return sum(1 for ch in s[:len(s)//2] if ch in vowels) == sum(1 for ch in s[len(s)//2:] if ch in vowels)
+        
