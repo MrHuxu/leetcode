2af7275ba4b4dc1cce6a8b303d8b2c6f396eeb16
xhu <hxtheone@gmail.com>
2024-01-22 21:29:17 +0800

    update daily challenge solution on 2024-01-22

--- problems/0645_set-mismatch/__main__.py
+++ problems/0645_set-mismatch/__main__.py
@@ -1,12 +1,11 @@
 class Solution:
     def findErrorNums(self, nums: List[int]) -> List[int]:
-        ret = [0, 0]
-        sum, visited, n = 0, set(), len(nums)
+        visited, n = set(), len(nums)
+        ret = [0, (1 + n) * n // 2]
         for num in nums:
             if num in visited:
                 ret[0] = num
             else:
                 visited.add(num)
-                sum += num
-        ret[1] = (1 + n) * n // 2 - sum
+                ret[1] -= num
         return ret
