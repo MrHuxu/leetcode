387cfcfe46068dc765535d85366b7967c1fe8eff
xhu <hxtheone@gmail.com>
2024-01-29 13:22:45 +0800

    completed daily challenge on 2024-01-29

---
+++ problems/232_implement-queue-using-stacks/__main__.py
@@ -0,0 +1,34 @@
+class MyQueue:
+
+    def __init__(self):
+        self.stack1 = []
+        self.stack2 = []
+        
+
+    def push(self, x: int) -> None:
+        while self.stack2:
+            self.stack1.append(self.stack2.pop(-1))
+        self.stack1.append(x)
+        
+
+    def pop(self) -> int:
+        while self.stack1:
+            self.stack2.append(self.stack1.pop(-1))
+        return self.stack2.pop(-1)
+        
+
+    def peek(self) -> int:
+        return self.stack1[0] if self.stack1 else self.stack2[-1]
+        
+
+    def empty(self) -> bool:
+        return not self.stack1 and not self.stack2
+        
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
