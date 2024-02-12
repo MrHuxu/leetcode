ad32e5b5e1d26d47445879b23becf0610e559407
xhu <hxtheone@gmail.com>
2024-02-12 18:50:22 +0800

    completed daily challenge on 2024-02-12

---
+++ problems/169_majority-element/__main__.py
@@ -0,0 +1,15 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        cnt, ret = 0, 0
+        for num in nums:
+            if cnt == 0:
+                ret = num
+            
+            if num == ret:
+                cnt += 1
+            else:
+                cnt -= 1
+        return ret
+    
+    def majorityElement2(self, nums: List[int]) -> int:
+        return max(Counter(nums).items(), key=lambda x: x[1])[0]
