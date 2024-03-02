e2baf2b210842cccf4bd74195d1834acc9b26f2f
xhu <hxtheone@gmail.com>
2024-03-03 00:03:51 +0800

    optimize code

--- problems/449_serialize-and-deserialize-bst/__main__.py
+++ problems/449_serialize-and-deserialize-bst/__main__.py
@@ -15,28 +15,30 @@ class Codec:
         """Encodes a tree to a single string."""
         if not root:
             return ""
-        ret = str(root.val)
-        if root.left:
-            ret += "," + self.serialize(root.left)
-        if root.right:
-            ret += "," + self.serialize(root.right)
-        return ret
+        return (
+            str(root.val)
+            + ("," + self.serialize(root.left) if root.left else "")
+            + ("," + self.serialize(root.right) if root.right else "")
+        )
 
     def deserialize(self, data: str) -> Optional[TreeNode]:
         """Decodes your encoded data to tree."""
-        return self.deserializeNums([int(d) for d in data.split(",")])
+        return (
+            None
+            if not data
+            else self.deserializeNums([int(d) for d in data.split(",")])
+        )
 
     def deserializeNums(self, nums: List[any]) -> Optional[TreeNode]:
         if not nums:
             return None
-        root = TreeNode(nums[0])
-        idx = 1
-        while idx < len(nums):
-            if nums[idx] > nums[0]:
-                break
+        root, idx = TreeNode(nums[0]), 1
+        while idx < len(nums) and nums[idx] < nums[0]:
             idx += 1
-        root.left = self.deserializeNums(nums[1:idx])
-        root.right = self.deserializeNums(nums[idx:])
+        root.left, root.right = (
+            self.deserializeNums(nums[1:idx]),
+            self.deserializeNums(nums[idx:]),
+        )
         return root
 
 
