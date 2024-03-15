9791e2760c5508454bd8844fc07c83e4551b5675
xhu <hxtheone@gmail.com>
2024-03-15 10:56:29 +0800

    update solution for daily challenge 2024-03-15

--- problems/238_product-of-array-except-self/__main__.py
+++ problems/238_product-of-array-except-self/__main__.py
@@ -4,8 +4,9 @@ class Solution:
         pre_product, post_product = [0] * n, [0] * n
         for i in range(n):
             pre_product[i] = nums[i] * (1 if i == 0 else pre_product[i - 1])
-        for i in reversed(range(n)):
-            post_product[i] = nums[i] * (1 if i == n - 1 else post_product[i + 1])
+            post_product[n - 1 - i] = nums[n - 1 - i] * (
+                1 if i == 0 else post_product[n - i]
+            )
         return [
             (1 if i == 0 else pre_product[i - 1])
             * (1 if i == n - 1 else post_product[i + 1])
