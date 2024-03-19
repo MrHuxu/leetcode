7e15b7425e2ec29c8a45f4321f5f565daab66465
xhu <hxtheone@gmail.com>
2024-03-19 12:30:16 +0800

    completed daily challenge on 2024-03-19

---
+++ task-scheduler.py
@@ -0,0 +1,11 @@
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        cnt = [0] * 26
+        for t in tasks:
+            cnt[ord(t) - ord('A')] += 1
+        cnt.sort(key=lambda x: -x)
+        chunk = cnt[0] - 1
+        idle = chunk * n
+        for i in range(1, 25):
+            idle -= min(chunk, cnt[i])
+        return len(tasks) + idle if idle >= 0 else len(tasks)
